0d7b4027eb5b1203956d5e2d7ba35ecc99f3b783
Kevin R Owens III <krowens@my.canyons.edu>
2021-11-28 22:54:56 -0800

    create and clean

---
+++ src/features/weatcherSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const weatcherSlice = createSlice({
+  name: sliceName,
+  initialState,
+  reducers: {},
+});
+
+export const {} = weatcherSlice.actions;
+export default weatcherSlice.reducer;
